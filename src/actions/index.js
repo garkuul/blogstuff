542e3a626c0525532817123f6e0811771e8d9c6d
Paulo Mendes <e.paulo.mendes@gmail.com>
2020-07-20 10:44:22 +0100

    memoize finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,11 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: 'FETCH_USER',
     payload: response.data,
   });
-};
+});
